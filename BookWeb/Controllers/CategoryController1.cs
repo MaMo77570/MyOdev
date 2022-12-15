af6fcadb0d6bcf0ad9bb7fc4e089e9f30c48ae1d
Marzooq Moftah <83363867+MaMo77570@users.noreply.github.com>
2022-12-15 16:51:32 +0300

    I Bootstrab to the Create file/ Index.cshtml

--- BookWeb/Controllers/CategoryController1.cs
+++ BookWeb/Controllers/CategoryController1.cs
@@ -1,12 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookWeb.Data;
+using BookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookWeb.Controllers
 {
     public class CategoryController1 : Controller
     {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController1(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            IEnumerable<Category> objCategoryList= _db.Categories;
+            return View(objCategoryList);
+        }
+        ///////////////////////////////////////////GET
+        public IActionResult Create()
+        {
+            
             return View();
+        }
+
+        ///////////////////////////////////////////POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name==obj.DiaplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order Cannot Exatly Match The Name.");
+            }
+
+            if (ModelState.IsValid) 
+            { 
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            }
+            return View(obj);
         }
     }
 }
