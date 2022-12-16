e481670e8ead392e395da17fd86b2636518f56e6
Marzooq Moftah <83363867+MaMo77570@users.noreply.github.com>
2022-12-16 17:19:38 +0300

    I create a new file for editting the Db that I have created before

--- BookWeb/Controllers/CategoryController1.cs
+++ BookWeb/Controllers/CategoryController1.cs
@@ -1,47 +1,99 @@
 ﻿using BookWeb.Data;
 using BookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace BookWeb.Controllers
+namespace BookWeb.Controllers;
+
+public class CategoryController1 : Controller
 {
-    public class CategoryController1 : Controller
+
+    private readonly ApplicationDbContext _db;
+
+    public CategoryController1(ApplicationDbContext db)
     {
+        _db = db;
+    }
+    public IActionResult Index()
+    {
+        IEnumerable<Category> objCategoryList= _db.Categories;
+        return View(objCategoryList);
+    }
 
-        private readonly ApplicationDbContext _db;
+    //GET
+    public IActionResult Create()
+    {
+        
+        return View();
+    }
 
-        public CategoryController1(ApplicationDbContext db)
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Category obj)
+    {
+        if(obj.Name==obj.DiaplayOrder.ToString())
         {
-            _db = db;
+            ModelState.AddModelError("name", "The Display Order Cannot Exatly Match The Name.");
         }
-        public IActionResult Index()
-        {
-            IEnumerable<Category> objCategoryList= _db.Categories;
-            return View(objCategoryList);
+
+        if (ModelState.IsValid) 
+        { 
+        _db.Categories.Add(obj);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
         }
-        ///////////////////////////////////////////GET
-        public IActionResult Create()
+        return View(obj);
+    }
+
+
+    //GET
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id==0)
         {
-            
-            return View();
+            return NotFound();
+        }
+        var categoryFormDb = _db.Categories.Find(id);
+        //var categoryFormDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+       // var categoryFormDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+        
+        if (categoryFormDb == null)
+        {
+            return NotFound();
         }
 
-        ///////////////////////////////////////////POST
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Category obj)
-        {
-            if(obj.Name==obj.DiaplayOrder.ToString())
-            {
-                ModelState.AddModelError("name", "The Display Order Cannot Exatly Match The Name.");
-            }
+        return View(categoryFormDb);
+    }
 
-            if (ModelState.IsValid) 
-            { 
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category obj)
+    {
+        if (obj.Name == obj.DiaplayOrder.ToString())
+        {
+            ModelState.AddModelError("name", "The Display Order Cannot Exatly Match The Name.");
+        }
+
+        if (ModelState.IsValid)
+        {
             _db.Categories.Add(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
-            }
-            return View(obj);
         }
+        return View(obj);
     }
+
+
+
+
+
+
+
+
+
+
+
 }
+
--- BookWeb/Models/Category.cs
+++ BookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookWeb.Models
 {
@@ -8,6 +9,8 @@ namespace BookWeb.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Diplay order must be between 1 and 100 !!")]
         public int DiaplayOrder { get; set; }
         public DateTime GetDateTime { get; set; }= DateTime.Now;
     }
