fd7315410bfc78abecaca0ce4404a843c7011dbc
Marzooq Moftah <83363867+MaMo77570@users.noreply.github.com>
2022-12-23 11:08:47 +0300

    I add Some file

--- BookWeb.Models/Company.cs
+++ BookWeb.Models/Company.cs
@@ -7,14 +7,15 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.Models
 {
-    public class CoverType
+    public class Company
     {
-        [Key]
         public int Id { get; set; }
-
-        [Display(Name = "Cover Type")]
         [Required]
-        [MaxLength(50)]
         public string Name { get; set; }
+        public string? StreetAddress { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? PostalCode { get; set; }
+        public string? PhoneNumber { get; set; }
     }
 }
