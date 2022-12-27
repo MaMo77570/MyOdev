a438b13023e65663258b529229c0ffcf0bd6c70a
Marzooq Moftah <83363867+MaMo77570@users.noreply.github.com>
2022-12-27 23:46:11 +0300

    I add API in my Project

---
+++ BookWeb/Areas/Admin/Controllers/BookApiController.cs
@@ -0,0 +1,50 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookApiController : ControllerBase
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: api/<BookApiController>
+        [HttpGet]
+        public List<Product> Get()
+        {
+            var products=db.Products.ToList();
+            return products;
+        }
+
+        // GET api/<BookApiController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<BookApiController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<BookApiController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<BookApiController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
